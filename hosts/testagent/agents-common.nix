e1f34520582cb210a70d6f064b7f64bd7faa86db
Joonas Rautiola <joonas.rautiola@unikie.com>
2025-03-14 15:09:12 +0200

    Rewrite testagents connection scripts
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- hosts/testagent/agents-common.nix
+++ hosts/testagent/agents-common.nix
@@ -12,6 +12,36 @@ let
   # Vendored in, as brainstem isn't suitable for nixpkgs packaging upstream:
   # https://github.com/NixOS/nixpkgs/pull/313643
   brainstem = pkgs.callPackage ../../pkgs/brainstem { };
+
+  connect-script = pkgs.writeShellApplication {
+    name = "connect";
+    text = # sh
+      ''
+        if [[ ! $1 =~ ^https?://[^/]+$ ]]; then
+          echo "ERROR: The URL should start with https and not have any subpath or trailing slash"
+          exit 1
+        fi
+
+        if [[ ! -f /var/lib/jenkins/jenkins.env ]]; then
+          sudo install -o jenkins -g jenkins -m 600 /dev/null /var/lib/jenkins/jenkins.env
+        fi
+        echo "CONTROLLER=$1" | sudo tee /var/lib/jenkins/jenkins.env
+        sudo systemctl restart start-agents.service
+
+        echo "Connected agents to the controller"
+      '';
+  };
+
+  disconnect-script = pkgs.writeShellApplication {
+    name = "disconnect";
+    text = # sh
+      ''
+        sudo systemctl stop start-agents.service
+        echo "CONTROLLER=" | sudo tee /var/lib/jenkins/jenkins.env
+
+        echo "Disconnected agents from the controller"
+      '';
+  };
 in
 {
   sops.secrets =
@@ -50,6 +80,8 @@ in
       usbsdmux
       grafana-loki
       (python3.withPackages (ps: with ps; [ pyserial ]))
+      connect-script
+      disconnect-script
     ]);
 
   # The Jenkins slave service is very barebones
@@ -69,43 +101,63 @@ in
 
   systemd.services =
     let
-      # verifies that the environment file is properly configured
-      # and downloads agent.jar from the controller
-      jenkins-setup-script = pkgs.writeShellScript "jenkins-setup.sh" ''
-        if [[ ! -f jenkins.env ]]; then
-          install -m 600 /dev/null jenkins.env
-          echo "CONTROLLER=" > jenkins.env
-          echo "Please add jenkins controller url to $(pwd)/jenkins.env"
-          exit 1
-        fi
-
-        source jenkins.env
-
-        if [[ -z "$CONTROLLER" ]]; then
-          echo "Variable CONTROLLER not set in $(pwd)/jenkins.env"
-          exit 1
-        fi
-
-        wget -O agent.jar "$CONTROLLER/jnlpJars/agent.jar"
-      '';
-
       # Helper function to create agent services for each hardware device
-      mkAgent =
-        device:
-        let
-          jenkins-connect-script =
+      mkAgent = device: {
+        bindsTo = [ "start-agents.service" ];
+        wantedBy = [ "start-agents.service" ];
+        after = [ "start-agents.service" ];
+
+        path =
+          [
+            brainstem
+            inputs.robot-framework.packages.${pkgs.system}.ghaf-robot
+          ]
+          ++ (with pkgs; [
+            curl
+            wget
+            jdk
+            git
+            bashInteractive
+            coreutils
+            util-linux
+            nix
+            zstd
+            jq
+            csvkit
+            sudo
+            openssh
+            iputils
+            netcat
+            python3
+            usbsdmux
+            grafana-loki
+          ]);
+
+        serviceConfig = {
+          Type = "simple";
+          User = "jenkins";
+          EnvironmentFile = "/var/lib/jenkins/jenkins.env";
+          WorkingDirectory = "/var/lib/jenkins";
+          Restart = "no";
+          SuccessExitStatus = [ 6 ];
+          ExecStart = toString (
             pkgs.writeShellScript "jenkins-connect.sh" # sh
               ''
-                # connects to controller with ssh, host key checking is disabled as it changes frequently
-                # grabs the secret from jenkins jnlp file and saves it to a variable
-                JENKINS_SECRET="$(
-                  ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${config.sops.secrets.ssh_host_ed25519_key.path}\
-                  ${config.networking.hostName}@''${CONTROLLER#*//} \
-                  "curl -H 'X-Forwarded-User: ${config.networking.hostName}' localhost:8081/computer/${device}/jenkins-agent.jnlp" |
-                  sed "s/.*<application-desc><argument>\([a-z0-9]*\).*/\1\n/"
-                )"
+                if [[ -z "$CONTROLLER" ]]; then
+                  echo "ERROR: Variable CONTROLLER not configured in $(pwd)/jenkins.env"
+                  exit 6
+                fi
 
                 mkdir -p "/var/lib/jenkins/agents/${device}"
+
+                # connects to controller with ssh, host key checking is disabled as it changes frequently
+                # grabs the secret from jenkins jnlp file and saves it to a variable
+                JNLP="$(
+                  ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${config.sops.secrets.ssh_host_ed25519_key.path} \
+                  ${config.networking.hostName}@''${CONTROLLER#*//} \
+                  "curl -H 'X-Forwarded-User: ${config.networking.hostName}' http://localhost:8081/computer/${device}/jenkins-agent.jnlp"
+                )"
+                JENKINS_SECRET="$(echo $JNLP | sed "s/.*<application-desc><argument>\([a-z0-9]*\).*/\1\n/")"
 
                 # opens a websocket connection to the jenkins controller from this agent
                 ${pkgs.jdk}/bin/java \
@@ -115,68 +167,36 @@ in
                   -secret "$JENKINS_SECRET" \
                   -workDir "/var/lib/jenkins/agents/${device}" \
                   -webSocket
-              '';
-        in
-        {
-          # agents require the setup service to run without errors before starting
-          requires = [ "setup-agents.service" ];
-          wantedBy = [ "setup-agents.service" ];
-          after = [ "setup-agents.service" ];
-
-          path =
-            [
-              brainstem
-              inputs.robot-framework.packages.${pkgs.system}.ghaf-robot
-            ]
-            ++ (with pkgs; [
-              curl
-              wget
-              jdk
-              git
-              bashInteractive
-              coreutils
-              util-linux
-              nix
-              zstd
-              jq
-              csvkit
-              sudo
-              openssh
-              iputils
-              netcat
-              python3
-              usbsdmux
-              grafana-loki
-            ]);
-
-          serviceConfig = {
-            Type = "simple";
-            User = "jenkins";
-            EnvironmentFile = "/var/lib/jenkins/jenkins.env";
-            WorkingDirectory = "/var/lib/jenkins";
-            ExecStart = "${jenkins-connect-script}";
-          };
-        };
-    in
-    {
-      # the setup service does not start automatically or it would fail activation
-      # of the system since jenkins.env is empty before manually set up
-      setup-agents = {
-        path = with pkgs; [ wget ];
-        serviceConfig = {
-          Type = "oneshot";
-          User = "jenkins";
-          RemainAfterExit = "yes";
-          WorkingDirectory = "/var/lib/jenkins";
-          ExecStart = "${jenkins-setup-script}";
+              ''
+          );
         };
       };
-
+    in
+    {
       # one agent per unique hardware device to act as a lock
       agent-orin-agx = mkAgent "orin-agx";
       agent-orin-nx = mkAgent "orin-nx";
       agent-riscv = mkAgent "riscv";
       agent-nuc = mkAgent "nuc";
       agent-lenovo-x1 = mkAgent "lenovo-x1";
+
+      start-agents = {
+        path = with pkgs; [ wget ];
+        serviceConfig = {
+          Type = "oneshot";
+          User = "jenkins";
+          RemainAfterExit = "yes";
+          WorkingDirectory = "/var/lib/jenkins";
+          ExecStart = toString (
+            pkgs.writeShellScript "start-agents.sh" # sh
+              ''
+                if [[ ! -f agent.jar ]]; then
+                  echo "Downloading agent.jar"
+                  wget -O "$CONTROLLER/jnlpJars/agent.jar"
+                fi
+              ''
+          );
+        };
+      };
     };
 }
