2dd5534f8ca46c131ce22118dc7bd170298aff55
Joonas Rautiola <joonas.rautiola@unikie.com>
2025-10-09 15:26:48 +0300

    temp, sbsign work
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- hosts/hetzci/signing.nix
+++ hosts/hetzci/signing.nix
@@ -9,10 +9,45 @@
 let
   selfPackages = self.packages.${pkgs.system};
 
+  pkcs11-provider = pkgs.pkcs11-provider.overrideAttrs rec {
+    version = "1.1";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "latchset";
+      repo = "pkcs11-provider";
+      tag = "v${version}";
+      fetchSubmodules = true;
+      hash = "sha256-QXEwDl6pk8G5ba8lD4uYw2QuD3qS/sgd1od8crHct2s=";
+    };
+  };
+
   proxyConfig = {
     PKCS11_PROXY_MODULE = "${selfPackages.pkcs11-proxy}/lib/libpkcs11-proxy.so";
     PKCS11_PROXY_TLS_PSK_FILE = config.sops.secrets.tls-pks-file.path;
     PKCS11_PROXY_SOCKET = "tls://nethsm-gateway.sumu.vedenemo.dev:2345";
+    OPENSSL_CONF = toString (
+      pkgs.writeText "openssl.cnf" # ini
+        ''
+          openssl_conf = openssl_init
+
+          [openssl_init]
+          providers = provider_sect
+
+          [provider_sect]
+          default = default_sect
+          pkcs11 = pkcs11_sect
+
+          # basic openssl functionality such as tls breaks when default provider is not present
+          [default_sect]
+          activate = 1
+
+          [pkcs11_sect]
+          activate = 1
+          module = "${pkcs11-provider}/lib/ossl-modules/pkcs11.so"
+          pkcs11-module-path = "${selfPackages.pkcs11-proxy}/lib/libpkcs11-proxy.so";
+        ''
+    );
+
   };
 in
 {
@@ -24,6 +59,7 @@ in
     (with pkgs; [
       opensc # pkcs11-tool
       cosign
+      openssl
     ])
     ++ [ selfPackages.systemd-sbsign ];
 
